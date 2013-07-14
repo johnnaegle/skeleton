32a8eea15a0ad94c1cbd82cf101894b440490b29
John Naegle <john.naegle@gmail.com>
2013-07-14 15:04:46 -0500

    basic skeleton app, gems and database configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skeleton::Application.config.secret_key_base = '1671a0d17d01a9800e08588db9a134363ff2b4fd5bad01e3c086e862f95f60855a1e5de4964da41f5c957690a0be04f3679bcfc5f0e6fcf94375251662d137f8'
